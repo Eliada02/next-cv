1954dcac0fa00740a457cced568db1a28f243620
Eliada Salla <eliadasalla20@gmail.com>
2025-09-03 16:53:45 +0200

    Header section finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 import Navbar from '@/components/navbar'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
     <>
     <Navbar/>
+    <Header/>
+    
     </>
   )
 
